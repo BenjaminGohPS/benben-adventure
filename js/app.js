77cc68704df8e829b25b6523264321aaa4c4bd80
benjamingohps <benjamingohps@gmail.com>
2024-10-06 10:39:58 +0800

    go shop, and go town button works

--- js/app.js
+++ js/app.js
@@ -18,20 +18,21 @@ const locations = [
     name: "Town",
     buttonText: ["Shop", "Forest", "Dragon"],
     // buttonFunction: [goShop, goForest, fightDragon],
+    text: "You are now standing at the town square. To your left is the store. Up ahead is the forest. To the right of the town, lies the fearsome Dragon you have heard from the whispers going around town.",
   },
   {
     name: "Shop",
-    buttonText: ["Shop", "Forest", "Dragon"],
-    // buttonFunction: [goShop, goForest, fightDragon],
+    buttonText: ["Buy Health", "Buy Weapon", "Town"],
+    // buttonFunction: [buyHealth, buyWeapon, goTown],
+    text: "Ah, an adventurer! Come now, what can I do for you?",
   },
 ];
 
 const monster = [
   {
     name: "Slime",
+    level: 1,
     health: 5,
-    attack: 1,
-    gold: 7,
   },
 ];
 
@@ -43,37 +44,72 @@ let xp = 0;
 /*----- Cached Element References  -----*/
 
 /*-------------- Functions -------------*/
-
+//CHECKS
 console.log(locations[0].buttonText[0]);
+console.log(locations[1].buttonText[0]);
 
 const init = () => {
   //code to init the game
-  //   button1.onclick = goShop;
-  //   button2.onclick = goForest;
-  //   button3.onclick = fightDragon;
+  console.log(`If initiation works, this text appears`);
+
+  button1.onclick = goShop;
+  button2.onclick = goForest;
+  button3.onclick = fightDragon;
 
   button1.innerText = locations[0].buttonText[0];
-  console.log(locations[0].buttonText[0]);
   button2.innerText = locations[0].buttonText[1];
   button3.innerText = locations[0].buttonText[2];
+
+  //   button2.onclick = goForest;
+  //   button3.onclick = fightDragon;
 };
 
 const render = () => {
   //code to render
 };
 
-// const goShop = () = {
-//     //function on button when clicked to go shop
+const goShop = () => {
+  //function on button when clicked to go shop
+  button1.innerText = locations[1].buttonText[0];
+  button2.innerText = locations[1].buttonText[1];
+  button3.innerText = locations[1].buttonText[2];
+  text.innerText = locations[1].text;
+  button3.onclick = goTown;
+};
+
+const goTown = () => {
+  //function on button when clicked to go town
+  button1.innerText = locations[0].buttonText[0];
+  button2.innerText = locations[0].buttonText[1];
+  button3.innerText = locations[0].buttonText[2];
+  text.innerText = locations[0].text;
+  button1.onclick = goShop;
+  button2.onclick = goForest;
+  button3.onclick = fightDragon;
+};
+
+const goForest = () => {
+  //function on button when clicked to go forest
+};
+
+const fightDragon = () => {
+  //function on button when clicked to go store
+};
+
+// const buyHealth = () => {
+//   //buy health
 // };
 
-// const goForest = () = {
-//     //function on button when clicked to go forest
-// };
-
-// const fightDragon = () = {
-//     //function on button when clicked to go store
+// const buyWeapon = () => {
+//   //buy weapon
 // };
 
 init();
+
 /*----------- Event Listeners ----------*/
 //button1.addEventListener('click', goStore);
+
+/*----------- PERSONAL NOTES ----------*/
+/*
+monster gold, monster attack -> will be randomise by math formula
+*/
