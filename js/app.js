0f9fdca4238b5bedf89342ed5792ff5bbce2fc77
benjamingohps <benjamingohps@gmail.com>
2024-10-06 11:32:40 +0800

    buyHp causes gold to go down, and hp to go up. Display text too.

--- js/app.js
+++ js/app.js
@@ -15,16 +15,30 @@ const text = document.getElementById("text");
 
 const locations = [
   {
+    //location[0]
     name: "Town",
     buttonText: ["Shop", "Forest", "Dragon"],
     // buttonFunction: [goShop, goForest, fightDragon],
     text: "You are now standing at the town square. To your left is the store. Up ahead is the forest. To the right of the town, lies the fearsome Dragon you have heard from the whispers going around town.",
   },
   {
+    //location[1]
     name: "Shop",
-    buttonText: ["Buy Health", "Buy Weapon", "Town"],
-    // buttonFunction: [buyHealth, buyWeapon, goTown],
+    buttonText: ["Town", "Buy HP", "Buy Weapon"],
+    // buttonFunction: [goTown, buyHp, buyWeapon, ],
     text: "Ah, an adventurer! Come now, what can I do for you?",
+  },
+  {
+    //location[2]
+    name: "Forest",
+    buttonText: ["Town", "Inner Forest", "Fight Slime"],
+    text: "You have enter the Forest. The view in front of you can only be describe as calm and serene. The way back to town is right behind you. Ahead lies the inner forest. Perhaps you can do some training here as Slimes are found abundantly here in the Forest",
+  },
+  {
+    //location[3]
+    name: "Fight",
+    buttonText: ["Attack", "Block", "Run"],
+    text: 'You have engage the monster.', // think of how to amend the text based on which monster I am attacking
   },
 ];
 
@@ -36,10 +50,16 @@ const monster = [
   },
 ];
 
+const weapons = [
+  { name: "trusty knuckles", power: 1 },
+  { name: "The Stick", power: 5 },
+];
 /*---------- Variables (state) ---------*/
 let hp = 100;
 let gold = 50;
 let xp = 0;
+let currentWeapon = weapons[0];
+console.log(weapons[0]);
 
 /*----- Cached Element References  -----*/
 
@@ -52,16 +72,13 @@ const init = () => {
   //code to init the game
   console.log(`If initiation works, this text appears`);
 
-  button1.onclick = goShop;
-  button2.onclick = goForest;
-  button3.onclick = fightDragon;
-
   button1.innerText = locations[0].buttonText[0];
   button2.innerText = locations[0].buttonText[1];
   button3.innerText = locations[0].buttonText[2];
 
-  //   button2.onclick = goForest;
-  //   button3.onclick = fightDragon;
+  button1.onclick = goShop;
+  button2.onclick = goForest;
+  button3.onclick = fightDragon;
 };
 
 const render = () => {
@@ -74,7 +91,9 @@ const goShop = () => {
   button2.innerText = locations[1].buttonText[1];
   button3.innerText = locations[1].buttonText[2];
   text.innerText = locations[1].text;
-  button3.onclick = goTown;
+  button1.onclick = goTown;
+  button2.onclick = buyHp;
+  button3.onclick = buyWeapon;
 };
 
 const goTown = () => {
@@ -90,19 +109,79 @@ const goTown = () => {
 
 const goForest = () => {
   //function on button when clicked to go forest
+  button1.innerText = locations[2].buttonText[0];
+  button2.innerText = locations[2].buttonText[1];
+  button3.innerText = locations[2].buttonText[2];
+  text.innerText = locations[2].text;
+  button1.onclick = goTown;
+  button2.onclick = goInnerForest;
+  button3.onclick = fightSlime;
+};
+
+const goInnerForest = () => {
+  //function on button when clicked to go inner forest
+};
+
+const fightSlime = () => {
+  //function on button when clicked to go store
+  /* ref
+  button1.innerText = locations[2].buttonText[0];
+  button2.innerText = locations[2].buttonText[1];
+  button3.innerText = locations[2].buttonText[2];
+  text.innerText = locations[2].text;
+  button1.onclick = goTown;
+  button2.onclick = goInnerForest;
+  button3.onclick = fightSlime;
+  */
+
+  button1.innerText = locations[3].buttonText[0];
+  button2.innerText = locations[3].buttonText[1];
+  button3.innerText = locations[3].buttonText[2];
+  text.innerText = `${locations[3].text} \n The Slime smiles at you while bouncing on the spot`;
+
+  button1.onclick = goAttack;
+  button2.onclick = goBlock;
+  button3.onclick = goTown;
+
+  /*
+  const monster = [
+    {
+      name: "Slime",
+      level: 1,
+      health: 5,
+    },
+  ];
+*/
 };
 
 const fightDragon = () => {
   //function on button when clicked to go store
 };
 
-// const buyHealth = () => {
-//   //buy health
-// };
+const goAttack = () => {
+  //attack codes
+};
 
-// const buyWeapon = () => {
-//   //buy weapon
-// };
+const goBlock = () => {
+  //block codes
+};
+
+const buyHp = () => {
+  //buy health
+  if (gold >= 10) {
+    gold -= 10;
+    hp += 10;
+    goldText.innerText = gold;
+    hpText.innerText = hp;
+  } else {
+    text.innerText =
+      '"Looks like you ran out of gold.", says the Shopkeeper, as he looks at you judgementally.';
+  }
+};
+
+const buyWeapon = () => {
+  //buy weapon
+};
 
 init();
 
